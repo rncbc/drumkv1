b5e56b2cd80c7ec4dc99e5d673a564025d7d716b
rncbc <rncbc@be9cb035-7082-4f78-a559-74415fa64963>
2014-01-25 18:11:20 +0000

    - Reverb has been added as a brand new effects stage, though   based on good old FreeVerb :). (EXPERIMENTAL)
    git-svn-id: http://svn.code.sf.net/p/drumkv1/code_attic/trunk@253 be9cb035-7082-4f78-a559-74415fa64963

--- src/drumkv1.cpp
+++ src/drumkv1.cpp
@@ -435,9 +435,9 @@ struct drumkv1_del
 struct drumkv1_rev
 {
 	float *wet;
-	float *feedb;
 	float *room;
 	float *damp;
+	float *feedb;
 	float *width;
 };
 
@@ -1152,9 +1152,9 @@ void drumkv1_impl::setParamPort ( drumkv1::ParamIndex index, float *pfParam )
 	case drumkv1::DEL1_BPMSYNC:   m_del.bpmsync   = pfParam; break;
 	case drumkv1::DEL1_BPMHOST:   m_del.bpmhost   = pfParam; break;
 	case drumkv1::REV1_WET:       m_rev.wet       = pfParam; break;
-	case drumkv1::REV1_FEEDB:     m_rev.feedb     = pfParam; break;
 	case drumkv1::REV1_ROOM:      m_rev.room      = pfParam; break;
 	case drumkv1::REV1_DAMP:      m_rev.damp      = pfParam; break;
+	case drumkv1::REV1_FEEDB:     m_rev.feedb     = pfParam; break;
 	case drumkv1::REV1_WIDTH:     m_rev.width     = pfParam; break;
 	case drumkv1::DYN1_COMPRESS:  m_dyn.compress  = pfParam; break;
 	case drumkv1::DYN1_LIMITER:   m_dyn.limiter   = pfParam; break;
@@ -1200,9 +1200,9 @@ float *drumkv1_impl::paramPort ( drumkv1::ParamIndex index )
 	case drumkv1::DEL1_BPMSYNC:   pfParam = m_del.bpmsync;   break;
 	case drumkv1::DEL1_BPMHOST:   pfParam = m_del.bpmhost;   break;
 	case drumkv1::REV1_WET:       pfParam = m_rev.wet;       break;
-	case drumkv1::REV1_FEEDB:     pfParam = m_rev.feedb;     break;
 	case drumkv1::REV1_ROOM:      pfParam = m_rev.room;      break;
 	case drumkv1::REV1_DAMP:      pfParam = m_rev.damp;      break;
+	case drumkv1::REV1_FEEDB:     pfParam = m_rev.feedb;     break;
 	case drumkv1::REV1_WIDTH:     pfParam = m_rev.width;     break;
 	case drumkv1::DYN1_COMPRESS:  pfParam = m_dyn.compress;  break;
 	case drumkv1::DYN1_LIMITER:   pfParam = m_dyn.limiter;   break;
