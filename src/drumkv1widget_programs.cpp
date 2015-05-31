2c32faaf976b96f0409bbf834fe6c4d5948e0684
rncbc <rncbc@gamma.rncbc.lan>
2015-05-31 21:52:14 +0100

    - Ongoing controllers infra-structure introdution (5).

--- src/drumkv1widget_programs.cpp
+++ src/drumkv1widget_programs.cpp
@@ -194,6 +194,7 @@ drumkv1widget_programs::drumkv1widget_programs ( QWidget *pParent )
 {
 	QTreeWidget::setColumnCount(2);
 
+	QTreeWidget::setRootIsDecorated(true);
 	QTreeWidget::setAlternatingRowColors(true);
 	QTreeWidget::setUniformRowHeights(true);
 	QTreeWidget::setAllColumnsShowFocus(true);
