e265245cd64d3b066275783e26c04f2e50a749cd
rncbc <rncbc@gamma.rncbc.lan>
2015-06-01 15:35:47 +0100

    - Ongoing controllers infra-structure introdution (6).

--- src/drumkv1.cpp
+++ src/drumkv1.cpp
@@ -1443,9 +1443,6 @@ void drumkv1_impl::process_midi ( uint8_t *data, uint32_t size )
 		const float pitchbend = float(key + (value << 7) - 0x2000) / 8192.0f;
 		m_ctl.pitchbend = drumkv1_pow2f(*m_def.pitchbend * pitchbend);
 	}
-	else
-	// flush controllers...
-	m_controls.flush();
 
 	// process pending controllers...
 	m_controls.process_dequeue();
