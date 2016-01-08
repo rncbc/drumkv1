c2cf7574137e293bdd093a7c684adf3a74387561
rncbc <rncbc@t430.rncbc.lan>
2016-01-08 11:43:16 +0000

    - Added application keywords to freedesktop.org's AppData.

--- src/drumkv1_config.h
+++ src/drumkv1_config.h
@@ -1,7 +1,7 @@
 // drumkv1_config.h
 //
 /****************************************************************************
-   Copyright (C) 2012-2015, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2012-2016, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -29,7 +29,7 @@
 
 #define DRUMKV1_SUBTITLE     "an old-school drum-kit sampler."
 #define DRUMKV1_WEBSITE      "http://drumkv1.sourceforge.net"
-#define DRUMKV1_COPYRIGHT    "Copyright (C) 2012-2015, rncbc aka Rui Nuno Capela. All rights reserved."
+#define DRUMKV1_COPYRIGHT    "Copyright (C) 2012-2016, rncbc aka Rui Nuno Capela. All rights reserved."
 
 #define DRUMKV1_DOMAIN	"rncbc.org"
 
