a25255babfe0f116cab5053c7a9cd91937628dfb
rncbc <rncbc@gamma.rncbc.lan>
2016-05-06 19:09:44 +0100

    - Ditched most of legacy cruft. (2)

--- src/drumkv1_lv2.cpp
+++ src/drumkv1_lv2.cpp
@@ -256,7 +256,7 @@ void drumkv1_lv2::run ( uint32_t nframes )
 						if (drumkv1::paramValue(drumkv1::LFO1_BPMSYNC) > 0.0f) {
 							const float bpm = drumkv1::paramValue(drumkv1::LFO1_BPM);
 							if (::fabsf(host_bpm - bpm) > 0.01f)
-								drumkv1::setParamValue(drumkv1::LFO1_BPM, host_bpm, true) // Cached!;
+								drumkv1::setParamValue(drumkv1::LFO1_BPM, host_bpm, true); // Cached!;
 						}
 						if (drumkv1::paramValue(drumkv1::DEL1_BPMSYNC) > 0.0f) {
 							const float bpm = drumkv1::paramValue(drumkv1::DEL1_BPM);
