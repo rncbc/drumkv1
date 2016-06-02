ce5081b3945ea6da5cce12b66e1d5b4635711548
rncbc <rncbc@t430.rncbc.lan>
2016-06-02 16:29:19 +0100

    - Element switching is now asynchrounous on both forms (JACK and LV2). (4)

--- src/drumkv1widget_jack.cpp
+++ src/drumkv1widget_jack.cpp
@@ -1,7 +1,7 @@
 // drumkv1widget_jack.cpp
 //
 /****************************************************************************
-   Copyright (C) 2012-2015, rncbc aka Rui Nuno Capela. All rights reserved.
+   Copyright (C) 2012-2016, rncbc aka Rui Nuno Capela. All rights reserved.
 
    This program is free software; you can redistribute it and/or
    modify it under the terms of the GNU General Public License
@@ -79,8 +79,6 @@ drumkv1widget_jack::drumkv1widget_jack ( drumkv1_jack *pDrumk )
 	//initPreset();
 	refreshElements();
 	activateElement();
-
-	updateParamValues(drumkv1::NUM_PARAMS);
 }
 
 
