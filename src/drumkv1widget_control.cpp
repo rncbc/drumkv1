2f621668a0f9eae5652c9dc0ab22c5bc3f3213f3
rncbc <rncbc@rncbc.org>
2017-12-28 23:32:41 +0000

    - Fixed several .ui header title comments.

--- src/drumkv1widget_control.cpp
+++ src/drumkv1widget_control.cpp
@@ -226,7 +226,7 @@ void drumkv1widget_control::closeEvent ( QCloseEvent *pCloseEvent )
 void drumkv1widget_control::setControlKey ( const drumkv1_controls::Key& key )
 {
 	// Anti-flooding guard timer < 3sec...
-	if (m_pEventTimer->elapsed() < 3000)
+	if (m_iDirtySetup == 0 && m_pEventTimer->elapsed() < 3000)
 		return;
 
 	setControlType(key.type());
