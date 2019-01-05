f0eb24c85cb8665a5ef7bf0634738b5a95264a41
rncbc <rncbc@rncbc.org>
2019-01-05 14:48:37 +0000

    [xkeybd] Keyboard input range implementation. (FINAL)

--- src/drumkv1.cpp
+++ src/drumkv1.cpp
@@ -1767,7 +1767,6 @@ void drumkv1_impl::process_midi ( uint8_t *data, uint32_t size )
 			const float pitchbend = float(key + (value << 7) - 0x2000) / 8192.0f;
 			m_ctl.pitchbend = drumkv1_pow2f(*m_def.pitchbend * pitchbend);
 		}
-
 	}
 
 	// process pending controllers...
