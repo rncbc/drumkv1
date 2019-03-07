697a1debdf37f4721efd1cefd3718ceefdd2dc79
rncbc <rncbc@rncbc.org>
2019-03-07 15:31:41 +0000

    - A gentlier shutdown for the JACK client standalone client. (bis)

--- src/drumkv1_jack.cpp
+++ src/drumkv1_jack.cpp
@@ -637,6 +637,19 @@ void drumkv1_jack::shutdown (void)
 }
 
 
+void drumkv1_jack::shutdown_close (void)
+{
+	m_activated = false;
+
+	if (m_client) {
+		::jack_client_close(m_client);
+		m_client = NULL;
+	}
+
+	close();
+}
+
+
 //-------------------------------------------------------------------------
 // drumkv1_jack_application -- Singleton application instance.
 //
@@ -1006,11 +1019,18 @@ void drumkv1_jack_application::shutdown (void)
 	emit shutdown_signal();
 }
 
+
 void drumkv1_jack_application::shutdown_slot (void)
 {
+	bool bQuit = true;
+
+	if (m_pDrumk)
+		m_pDrumk->shutdown_close();
+
 	if (m_pWidget)
-		m_pWidget->close();
-	if (m_pApp)
+		bQuit = m_pWidget->queryClose();
+
+	if (m_pApp && bQuit)
 		m_pApp->quit();
 }
 
