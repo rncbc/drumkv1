06b3d0da63b295c46ca6a82cf216fcf00730f3a1
rncbc <rncbc@rncbc.org>
2022-11-25 16:15:55 +0000

    - Mitigate NSM sending lots of dirty messages on SIGTERM signal (EXPERIMENTAL) (GH issue #41 revisited).

--- src/drumkv1widget_jack.cpp
+++ src/drumkv1widget_jack.cpp
@@ -145,8 +145,8 @@ void drumkv1widget_jack::updateDirtyPreset ( bool bDirtyPreset )
 	drumkv1widget::updateDirtyPreset(bDirtyPreset);
 
 #ifdef CONFIG_NSM
-	if (m_pNsmClient && m_pNsmClient->is_active())
-		m_pNsmClient->dirty(true); // as far as NSM goes, we're always filthy!
+	if (m_pNsmClient && m_pNsmClient->is_active() && bDirtyPreset)
+		m_pNsmClient->dirty(true);
 #endif
 }
 
