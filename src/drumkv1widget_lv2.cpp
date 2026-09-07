6807620fc8cd31e0579081f37ce4fcbfbfe2f4c4
rncbc <rncbc@rncbc.org>
2026-09-07 09:36:05 +0100

    - Update factory presets and resp. paths.

--- src/drumkv1widget_lv2.cpp
+++ src/drumkv1widget_lv2.cpp
@@ -33,10 +33,6 @@
 
 #include <QStyleFactory>
 
-#ifndef CONFIG_BINDIR
-#define CONFIG_BINDIR	CONFIG_PREFIX "/bin"
-#endif
-
 #ifndef CONFIG_DATADIR
 #define CONFIG_DATADIR	CONFIG_PREFIX "/share"
 #endif
@@ -69,10 +65,7 @@ drumkv1widget_lv2::drumkv1widget_lv2 ( drumkv1_lv2 *pDrumk,
 	QApplication *pApp = drumkv1_lv2::qapp_instance();
 	if (pApp) {
 		// Special style paths...
-		QString sPluginsPath = pApp->applicationDirPath();
-		sPluginsPath.remove(CONFIG_BINDIR);
-		sPluginsPath.append(CONFIG_PLUGINSDIR);
-		if (QDir(sPluginsPath).exists())
+		if (QDir(CONFIG_PLUGINSDIR).exists())
 			pApp->addLibraryPath(CONFIG_PLUGINSDIR);
 	}
 
@@ -80,8 +73,7 @@ drumkv1widget_lv2::drumkv1widget_lv2 ( drumkv1_lv2 *pDrumk,
 	drumkv1_config *pConfig = drumkv1_config::getInstance();
 	if (pConfig) {
 		const QChar sep = QDir::separator();
-		QString sPalettePath = QApplication::applicationDirPath();
-		sPalettePath.remove(CONFIG_BINDIR);
+		QString sPalettePath;
 		sPalettePath.append(CONFIG_DATADIR);
 		sPalettePath.append(sep);
 		sPalettePath.append(PROJECT_NAME);
